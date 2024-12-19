3d8028b70af2c1a9ff4c4e0ce58c35843f1897eb
Sonu Kumar <nandanshah418@gmail.com>
2024-12-19 17:53:46 +0530

    Final commit

--- models/File.js
+++ models/File.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+
+require('dotenv').config();
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -15,6 +18,33 @@ const fileSchema = new mongoose.Schema({
         type:String,
     }
 });
+
+//Post middlware
+fileSchema.post("save", async function(doc) {
+    try{
+        //Transporter create
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            }
+        })
+    }
+    catch(error){
+        console.error(error);
+    }
+
+    //Send mail
+    let info = await transporter.sendMail({
+        from:`Nandan`,
+        to: doc.email,
+        subject: "New file upload in cloudinary",
+        html:`<h2>Hello </h2> <p>File Uploaded</p>`,
+    })
+
+    console.log("INFO", info)
+})
 
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
